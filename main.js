0556f9f65c64dffc7bb23e8ea3439dbc2ce7906d
forzajose <forzajose@gmail.com>
2023-05-26 17:49:31 +0300

    Add tinyMCE text editor

--- main.js
+++ main.js
@@ -1,25 +1,61 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+ /* Import TinyMCE */
+ import tinymce from 'tinymce';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+ /* Default icons are required for TinyMCE 5.3 or above */
+ import 'tinymce/icons/default';
+ import 'tinymce/models/dom/model';
 
-setupCounter(document.querySelector('#counter'))
-// console.log('hello world');
+
+ /* A theme is also required */
+ import 'tinymce/themes/silver';
+
+ /* Import the skin */
+ import 'tinymce/skins/ui/oxide/skin.css';
+
+ /* Import plugins */
+
+import 'tinymce/models/dom/model';
+import 'tinymce/plugins/preview'
+import 'tinymce/plugins/importcss'
+import 'tinymce/plugins/searchreplace'
+import 'tinymce/plugins/autolink'
+import 'tinymce/plugins/autosave'
+import 'tinymce/plugins/save'
+import 'tinymce/plugins/directionality'
+import 'tinymce/plugins/code'
+import 'tinymce/plugins/visualblocks'
+import 'tinymce/plugins/image'
+import 'tinymce/plugins/visualchars'
+import 'tinymce/plugins/fullscreen'
+import 'tinymce/plugins/link'
+import 'tinymce/plugins/media'
+import 'tinymce/plugins/template'
+import 'tinymce/plugins/codesample'
+import 'tinymce/plugins/table'
+import 'tinymce/plugins/charmap'
+import 'tinymce/plugins/pagebreak'
+import 'tinymce/plugins/nonbreaking'
+import 'tinymce/plugins/anchor'
+import 'tinymce/plugins/insertdatetime'
+import 'tinymce/plugins/advlist'
+import 'tinymce/plugins/lists'
+import 'tinymce/plugins/wordcount'
+import 'tinymce/plugins/help'
+import 'tinymce/plugins/quickbars'
+import 'tinymce/plugins/emoticons'
+import 'tinymce/plugins/emoticons/js/emojis.js';
+
+ /* Import content css */
+ import contentUiCss from 'tinymce/skins/ui/oxide/content.css';
+ import contentCss from 'tinymce/skins/content/default/content.css';
+
+ /* Initialize TinyMCE */
+
+   tinymce.init({
+     selector: '#tinymce',
+      plugins: 'preview importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media template codesample table charmap pagebreak nonbreaking anchor insertdatetime advlist lists wordcount help charmap quickbars emoticons',
+      toolbar: 'bold italic | bullist numlist | link emoticons',
+     skin: 'tinymce-5-dark',
+     content_css: false,
+     content_style: contentUiCss.toString() + '\n' + contentCss.toString(),
+   });
